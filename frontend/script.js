d2ff32653590e8b8f9a1107de90540cdbf7beeab
Timi <timeaher@gmail.com>
2022-10-12 11:16:03 +0200

    change in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,40 +1,36 @@
-console.log("loaded");
+const apiKey = "ARaWUHtGMQKP1vfXovbC8C2H88QwcfEJhkBuJiPW";
+let apiDate = "2007-04-01";
+let fetchUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${apiDate}`;
 
-const apiKey =o5p85oefcjQCyxabLFhNhPPKAx47arvcVYaBMefU;
-let apiDate="2022-10-12";
-let fetchURL=`https://api.nasa.gov/planetary/apod?api_key=$(apiKey)&date=$(apiDate)`;
+// fetch(fetchUrl).then(function (response) {
+//     return response.json();
+// }).then(function (responseJson){
+//     console.log(responseJson);
+// })
 
-/*fetch(fetchURL).then(function(response){
-   console.log("response");
-    console.log(response.json())
-    return.return.json(;)
-}).then function(responseJson){
-    console-log(responseJson);})*/
+// fetch(fetchUrl)
+//     .then(response => response.json())
+//     .then(responseJson => console.log(responseJson))
 
-
-//fetch(fetchURL)
-    .then(response => response.json() )
-    .then(responseJson => console.log(responseJson))//
-
-async function fetchNasa(){
-    const response =await fetch(fetchURL)
-    //console.log(response);//
-    const responseJson= await response.json();
-    //console.log(responseJson);//
+async function fetchNasa () {
+    const response = await fetch(fetchUrl);
+    // console.log(response);
+    const responseJson = await response.json();
+    // console.log(responseJson);
     return responseJson;
-    
 }
 
-async function loadEvent(){
+async function loadEvent () {
     let data = await fetchNasa();
-    console.log("data:", data);
+    console.log("data: ",data);
 
-    const rootElement=document.querySelector("#root");
+    const rootElement = document.querySelector("#root");
     rootElement.insertAdjacentHTML("beforeend",`
-    <h1> $data.title</h1>
-    <h2> $data.date</h2>
-    <p> $data.explanation</p>
-    <img src= $data.url)>
+        <h1>${data.title}</h1>
+        <h2>${data.date}</h2>
+        <p>${data.explanation}</p>
+        <img src="${data.url}">
+    `)
 }
 window.addEventListener("load",loadEvent);
     
